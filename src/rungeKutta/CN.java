a3d0371ea6d12065cc1ca029b52ac655058ee4f1
Daniel Santos <danielnd14@gmail.com>
2017-12-05 09:03:53 -0200

    alterando formataçao

--- src/rungeKutta/CN.java
+++ src/rungeKutta/CN.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class CN {
     public static void main(String args[]) {
         long time = System.currentTimeMillis();
-        System.out.println("================================================================================");
+        System.out.println("================================================================================\n");
         BigDecimal dois = new BigDecimal(2);
         BigDecimal um = new BigDecimal(1);
         BigDecimal tres = new BigDecimal(3);
@@ -37,8 +37,8 @@ public class CN {
                 new BigDecimal(3.0),
                 new BigDecimal(20.0),
                 (x, y) ->
-                       new BigDecimal(
-                               Math.sqrt(x.doubleValue())).add(um).multiply(dois));/*y' = ((x¹/²)+1)y*/
+                        new BigDecimal(
+                                Math.sqrt(x.doubleValue())).add(um).multiply(dois));/*y' = ((x¹/²)+1)y*/
 
         expressaoImprimivel = "y' = ((x¹/²)+1)y";
         imprimir(expressaoImprimivel, valor);
@@ -49,7 +49,7 @@ public class CN {
                 new BigDecimal(2.0),
                 new BigDecimal(50.0),
                 (x, y) ->
-                       x.pow(2).subtract(tres.multiply(x)).subtract(um).subtract(y));/*y' = x²-3x-1-y*/
+                        x.pow(2).subtract(tres.multiply(x)).subtract(um).subtract(y));/*y' = x²-3x-1-y*/
 
         expressaoImprimivel = "y' = x²-3x-1-y";
         imprimir(expressaoImprimivel, valor);
@@ -59,6 +59,6 @@ public class CN {
     }
 
     private static void imprimir(String expressaoImprimivel, BigDecimal[] valor) {
-        System.out.println(expressaoImprimivel+" ->\t\t\tX1:\t" + valor[0].doubleValue() + "\t|\tY1:\t" + valor[1].doubleValue()+"\n");
+        System.out.println(expressaoImprimivel + " ->\t\t\tX1:\t" + valor[0].doubleValue() + "\t|\tY1:\t" + valor[1].doubleValue() + "\n");
     }
 }
